0eb7d186877338374e07aadb383e602ad7b37fb6
Kevin Aguilar <kevin.aguilar@interaction.cr>
2018-04-03 09:51:57 -0600

    Nodemon test

--- arquitectura/server.js
+++ arquitectura/server.js
@@ -7,7 +7,7 @@ const port = 3000;
 const serveStatic = require('serve-static');
 // Se exporta la conexión de nodejs
 const connect = require('connect');
-
+// se exporta nodemon, cuya tarea es crea el servidor del back end
 const nodemon = require('nodemon');
 
 // Se establece la conexion y el puerto en el que la aplicación va a correr
@@ -15,16 +15,18 @@ connect().use(serveStatic(__dirname)).listen(port, () => {
   console.log('El servidor esta levantado dentro del puerto ' + port);
 });
 
+// Se establece cual va a ser el archivo que va a conectar nodemon
 nodemon({
   script: 'api/index.js',
   ext: 'js'
 });
 
-nodemon.on('start', function () {
-  console.log('App has started');
-}).on('quit', function () {
+// Creamos la conexion del backend con Nodemon
+nodemon.on('start', () => {
+  console.log('La aplicación ha sido levantada correctamente');
+}).on('quit', () => {
   console.log('App has quit');
   process.exit();
-}).on('restart', function (files) {
+}).on('restart', (files) => {
   console.log('App restarted due to: ', files);
 });
